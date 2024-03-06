32c258112fa02dfef10152da10c719b7cbb00134
Christian Boffill <christian.boffill@gmail.com>
2024-03-06 17:48:08 -0500

    revalidate by path

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -3,14 +3,17 @@ import { revalidatePath } from 'next/cache'
 
 export async function POST(request: NextRequest) {
   const res = await request.json()
-  console.log("🚀 ~ POST ~ res:", res)
+  const path = res.entry.path
   const secret = request.nextUrl.searchParams.get('secret')
-  // const path = request.nextUrl.searchParams.get('tag')
 
   if (secret !== process.env.REVALIDATE_SECRET) {
     return new Response('Invalid token', { status: 401 })
   }
 
-  // revalidatePath(path)
+  if (!path || path?.length === 0) {
+    return new Response('Invalid path', { status: 400 })
+  }
+
+  revalidatePath(path)
   return Response.json({ revalidated: true, now: Date.now() })
 }
